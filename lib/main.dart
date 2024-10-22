9f3ce8e2caed0ad94c49955ec6e0dc46fcb3486e
Wesley Heeringa <wesleyheeringa@hotmail.com>
2024-10-22 23:19:59 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+// Views
+import 'package:shedule_app/views/home.dart';
+
+void main() {
+  runApp(const AppRoot());
+}
+
+class AppRoot extends StatelessWidget {
+  const AppRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
